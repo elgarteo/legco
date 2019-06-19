ac0aa42917c5d3bfd393c879a7b0a82a6550d51f
Elgar Teo <elgarteo95@gmail.com>
2019-06-19 13:57:09 +0800

    Fix bug in utils-misc.R

--- R/utils-misc.R
+++ R/utils-misc.R
@@ -5,13 +5,14 @@ generate_filter <- function(var_name, var_values) {
   } else {
     filter_args <- paste0(var_name, " eq '", var_values, "'")
   }
-  filter_args <- paste(filter_args, collapse = " or ")
+  filter_args <- paste0("(", paste(filter_args, collapse = " or "), ")")
   filter_args
 }
 
 ## Function to unify format of column names (e.g. from 'id_name' to 'IdName')
 unify_colnames <- function(col_names) {
   sapply(col_names, function(x) {
+    x <- gsub("id$", "ID", x)
     x <- unlist(strsplit(x, "_"))
     x <- paste(paste0(toupper(substring(x, 1, 1)), substring(x, 2)), collapse = "")
   })
