4e0399ad3306b8c4c09e5fe803338ebd31194c0f
Elgar Teo <elgarteo95@gmail.com>
2019-06-19 13:57:09 +0800

    Fix bug in utils-misc.R

--- R/utils-misc.R
+++ R/utils-misc.R
@@ -5,7 +5,7 @@ generate_filter <- function(var_name, var_values) {
   } else {
     filter_args <- paste0(var_name, " eq '", var_values, "'")
   }
-  filter_args <- paste(filter_args, collapse = " or ")
+  filter_args <- paste0("(", paste(filter_args, collapse = " or "), ")")
   filter_args
 }
 
